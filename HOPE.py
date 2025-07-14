d22ff5f130dc4c33bfd22940a68032e92744aa81
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-07-14 19:28:14 +0100

    Delete HOPE.py

--- HOPE.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة HOPE.cpython-312.so
-import HOPE
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(HOPE, 'main'):
-    HOPE.main()
-else:
-    print("تم استيراد HOPE بنجاح، ولكن لا توجد دالة main للتشغيل.")
